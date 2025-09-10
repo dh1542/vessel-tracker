9dbcc6fec880b478999ef9b6c293a9576f11e7d3
Dominik Hoffmann <dominik151099@outlook.de>
2025-09-10 23:08:52 +0200

    database writer

--- AIS/database_writer.go
+++ AIS/database_writer.go
@@ -1,21 +1,63 @@
 package main
 
-
 import (
-	"fmt"
 	"database/sql"
+	"fmt"
+
 	_ "github.com/lib/pq"
 )
 
-const(
-	host = "localhost"
-	port = 5432
-	user = "postgres"
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
 	password = ""
-	dbname = "postgres"
+	dbname   = "postgres"
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db := connectToDB()
+	setUpDBTables(db)
+
+	fmt.Println("Database connected successfully")\
+
 }
 
+func connectToDB() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func setUpDBTables(db *sql.DB) {
+
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS ships (
+		id SERIAL PRIMARY KEY,
+		mmsi BIGINT UNIQUE,
+		ship_name VARCHAR(255),
+		latitude DOUBLE PRECISION,
+		longitude DOUBLE PRECISION,
+		timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP`
+	);
+	
+	res, err = db.Exec(createTableQuery)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Table created or already exists:", res)
+
+	
+}
