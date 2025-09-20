6eac4a8159ac6779219201170fef5405c97714d3
Dominik Hoffmann <dominik151099@outlook.de>
2025-09-20 11:07:01 +0200

    navBar wip

--- vessel-tracker-ui/src/components/navigationBar.tsx
+++ vessel-tracker-ui/src/components/navigationBar.tsx
@@ -1,53 +1,39 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
 import { Link } from "@heroui/link";
 import { Button } from "@heroui/button";
+import {Image} from "@heroui/image";
 
 
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
 
 export default function NavigationBar() {
   return (
     <Navbar>
-      <NavbarBrand>
-        <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
+      <NavbarBrand justify="start" className="flex gap-2">
+        <Image src="/vessel-tracker-logo.png" alt="Vessel Tracker Logo" width={70} height={70} />
+        <p className="font-bold text-inherit">Vessel-Tracker  </p>
+        
+        <p className="text-inherit text-sm"> by dh1542</p>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarContent className="hidden sm:flex gap-7" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
+          <Link  href="#">
+            Options
           </Link>
         </NavbarItem>
-        <NavbarItem isActive>
+        <NavbarItem>
           <Link aria-current="page" href="#">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
+            Map Layers
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
+        <NavbarItem className="hidden lg:flex ">
+            <Link isExternal href="https://github.com/dh1542/vessel-tracker" >
+                 <Image src="/github-mark.png" alt="Github Logo"  width={20} height={20} />
+                 <span>&nbsp;</span>
+                 <p className="text-black"> by dh1542</p>
+            </Link>
+          
         </NavbarItem>
       </NavbarContent>
     </Navbar>
