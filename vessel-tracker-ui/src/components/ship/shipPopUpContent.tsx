9c71a83a531182d070e383073e765ebffa0a3394
Dominik Hoffmann <dominik151099@outlook.de>
2025-09-28 09:16:34 +0200

    Pop Up

--- vessel-tracker-ui/src/components/ship/shipPopUpContent.tsx
+++ vessel-tracker-ui/src/components/ship/shipPopUpContent.tsx
@@ -14,35 +14,37 @@ export default function ShipPopUpContent(
   props: ShipComponentProps
 ): JSX.Element {
   return (
-    <Card className="w-bg-white shadow-2xs" fullWidth={true} shadow="lg">
-      <Table>
-        <TableHeader>
-          <TableColumn>NAME</TableColumn>
-          <TableColumn>VALUE</TableColumn>
-        </TableHeader>
-        <TableBody>
-          <TableRow key="1">
-            <TableCell>Ship Name</TableCell>
-            <TableCell>{props.name}</TableCell>
-          </TableRow>
-          <TableRow key="2">
-            <TableCell>Destination</TableCell>
-            <TableCell>{props.destination}</TableCell>
-          </TableRow>
-          <TableRow key="3">
-            <TableCell>MMSI</TableCell>
-            <TableCell>{props.mmsi}</TableCell>
-          </TableRow>
-          <TableRow key="4">
-            <TableCell>Latitude</TableCell>
-            <TableCell>{props.position.latitude}</TableCell>
-          </TableRow>
-          <TableRow key="5">
-            <TableCell>Longitude</TableCell>
-            <TableCell>{props.position.longitude}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </Card>
+    <div className="w-bg-white shadow-2xs">
+      <Card fullWidth={true} shadow="lg">
+        <Table>
+          <TableHeader>
+            <TableColumn>NAME</TableColumn>
+            <TableColumn>VALUE</TableColumn>
+          </TableHeader>
+          <TableBody>
+            <TableRow key="1">
+              <TableCell>Ship Name</TableCell>
+              <TableCell>{props.name}</TableCell>
+            </TableRow>
+            <TableRow key="2">
+              <TableCell>Destination</TableCell>
+              <TableCell>{props.destination}</TableCell>
+            </TableRow>
+            <TableRow key="3">
+              <TableCell>MMSI</TableCell>
+              <TableCell>{props.mmsi}</TableCell>
+            </TableRow>
+            <TableRow key="4">
+              <TableCell>Latitude</TableCell>
+              <TableCell>{props.position.latitude}</TableCell>
+            </TableRow>
+            <TableRow key="5">
+              <TableCell>Longitude</TableCell>
+              <TableCell>{props.position.longitude}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Card>
+    </div>
   );
 }
