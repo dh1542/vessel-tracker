cd8aacc7fb74e52764f004249bf7b7570ef16058
Dominik Hoffmann <dominik151099@outlook.de>
2025-09-28 10:24:09 +0200

    image scraping via duckduckgo

--- AIS/api/scraper/imageScraper.go
+++ AIS/api/scraper/imageScraper.go
@@ -2,6 +2,8 @@ package scraper
 
 import (
 	"fmt"
+	"log"
+	"net/url"
 
 	"github.com/gocolly/colly"
 )
@@ -9,14 +11,23 @@ import (
 // ScrapeImageUrlForShipName
 // https://www.zenrows.com/blog/web-scraping-golang#get-page-html/**
 func ScrapeImageUrlForShipName(shipName string) string {
+	query := shipName + " ship"
+	url := "https://duckduckgo.com/?q=" + url.QueryEscape(query) + "&iax=images&ia=images"
+	fmt.Println(url)
+
 	c := colly.NewCollector(
-		colly.AllowedDomains("https://images.google.com/"))
+		colly.UserAgent("Mozilla/5.0 (compatible; CollyBot/1.0; +https://github.com/gocolly/colly)"),
+		colly.AllowedDomains("duckduckgo.com", "www.duckduckgo.com"),
+	)
 
-	c.Visit("https://images.google.com/")
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting: ", r.URL)
+	c.OnHTML("body[div]", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
 	})
 
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Error:", err)
+	})
+
+	c.Visit(url)
 	return ""
 }
